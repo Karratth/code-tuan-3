d1efa326b23f16af628928888a73300f8fe58f9f
hv <hv@PM309-14>
2023-04-01 09:50:23 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int sum = 0;
+            //foreach(int i in number)
+            //{
+            //    sum += i;
+            // }
+            //Console.WriteLine("ket qua la " + sum);
+            //Console.ReadKey();
+
+            int n;
+            Console.WriteLine("nhap phan tu: ");
+            n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("nhap phan tu thu " + (i + 1) + ": ");
+                numbers[i] = int.Parse(Console.ReadLine());
+
+            }
+            Array.Sort(numbers);
+            Array.Reverse(numbers);  
+            Console.WriteLine("mang sau khi sap xep: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
